b2b3f4a9764e90ef5979bd84dbd3a8629f20000c
Nagesh Mishra <nagesh3.13@gmail.com.com>
2016-08-26 23:18:34 +0530

    Initial commit for static files with yeoman build configuration

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name staticBlogApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the staticBlogApp
+ */
+angular.module('staticBlogApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name staticBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the staticBlogApp
+ */
+angular.module('staticBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
